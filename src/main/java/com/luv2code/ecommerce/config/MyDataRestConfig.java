53cfcb795488ba2619dffee9181b51b81bc3e1fe
Artur Sobieraj <artur.sobieraj126@gmail.com>
2023-09-03 20:37:48 +0200

    Final push

--- src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -1,47 +1,59 @@
 package com.luv2code.ecommerce.config;
 
-import com.luv2code.ecommerce.entity.Product;
-import com.luv2code.ecommerce.entity.ProductCategory;
+import com.luv2code.ecommerce.entity.*;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
-@Configuration
-public class MyDataRestConfig implements RepositoryRestConfigurer {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
-    @Autowired
-    private EntityManger entityManger;
+@Configuration
+@RequiredArgsConstructor
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    private final EntityManager entityManager;
+
+    @Value("${allowed.origins}")
+    private String[] allowedOrigins;
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
 
-        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
-
-        config.getExposureConfiguration()
-                .forDomainType(Product.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
-
-        config.getExposureConfiguration()
-                .forDomainType(ProductCategory.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
-
+        disableHttpMethods(Product.class, config, theUnsupportedActions);
+        disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
+        disableHttpMethods(Country.class, config, theUnsupportedActions);
+        disableHttpMethods(State.class, config, theUnsupportedActions);
+        disableHttpMethods(Order.class, config, theUnsupportedActions);
         exposeIds(config);
+
+        cors.addMapping(config.getBasePath() + "/**").allowedOrigins(allowedOrigins);
     }
 
     private void exposeIds(RepositoryRestConfiguration config) {
-        Set<EntityType<?>> entities = entityManger.getMetaModel().getEntities();
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
 
-        List<Class> entityClasses = new ArrayList<>();
+        List<Class<?>> entityClasses = new ArrayList<>();
 
-        for (EntityType tempEntityType : entities) {
+        for (EntityType<?> tempEntityType : entities) {
             entityClasses.add(tempEntityType.getJavaType());
         }
 
-        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        Class<?>[] domainTypes = entityClasses.toArray(new Class[0]);
         config.exposeIdsFor(domainTypes);
+    }
+
+    private void disableHttpMethods(Class<?> clazz, RepositoryRestConfiguration config, HttpMethod[] unsupportedMethods) {
+        config.getExposureConfiguration()
+                .forDomainType(clazz)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods));
     }
 }
---
+++ src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
@@ -0,0 +1,37 @@
+package com.luv2code.ecommerce.controller;
+
+import com.luv2code.ecommerce.dto.PaymentInfo;
+import com.luv2code.ecommerce.dto.Purchase;
+import com.luv2code.ecommerce.dto.PurchaseResponse;
+import com.luv2code.ecommerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+    private final CheckoutService checkoutService;
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    @Autowired
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+        return checkoutService.placeOrder(purchase);
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+        return new ResponseEntity<>(paymentIntent.toJson(), HttpStatus.OK);
+    }
+}
